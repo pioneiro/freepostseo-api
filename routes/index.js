af323d122921f13d17d8e2685b173e5ca7208a66
Soumyajit Das <mepioneiro@gmail.com>
2021-06-03 16:15:48 +0530

    Included YouTube MP3 Downloader

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 
 import shorturl from "./shorturl.js";
 import scrapper from "./scrapper.js";
+import youtubemp3 from "./youtubemp3.js";
 
 const router = Router();
 
 router.use("/tools/shorturl", shorturl);
 router.use("/tools/scrapper", scrapper);
+router.use("/tools/youtubemp3", youtubemp3);
 
 router.get("/", (req, res) => {
 	res.send("Root");
---
+++ routes/youtubemp3.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+
+import youtubemp3 from "../lib/utils/youtubemp3.js";
+
+const router = Router();
+
+router.post("/", (req, res) => {
+	youtubemp3(req.body.url, (data) => {
+		if (data.error) res.json(data);
+		else {
+			const { fileName, readStream } = data;
+
+			res.setHeader("Content-Type", "audio/mpeg");
+			res.setHeader(
+				"Content-Disposition",
+				`attachment;\ filename="${fileName}"`
+			);
+
+			readStream.pipe(res);
+		}
+	});
+});
+
+export default router;
