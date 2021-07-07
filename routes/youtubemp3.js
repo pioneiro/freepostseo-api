e3f96edd41c350c8a431dfb809b3af84218e8c9e
Soumyajit Das <mepioneiro@gmail.com>
2021-07-07 23:15:42 +0530

    encode filename and error response modification

--- routes/youtubemp3.js
+++ routes/youtubemp3.js
@@ -5,22 +5,16 @@ import youtubemp3 from "../lib/utils/youtubemp3.js";
 const router = Router();
 
 router.post("/", (req, res) => {
-	youtubemp3(req.body.url, (data) => {
-		if (data.error) res.json(data);
-		else {
-			const { fileName, readStream } = data;
+  youtubemp3(req.body.url, ({ error, fileName, readStream }) => {
+    if (error) return res.json({ error });
 
-			res.setHeader("Access-Control-Expose-Headers", "filename");
-			res.setHeader("Content-Type", "audio/mpeg");
-			res.setHeader("filename", fileName);
-			res.setHeader(
-				"Content-Disposition",
-				`attachment;\ filename="${fileName}"`
-			);
+    res.setHeader("Access-Control-Expose-Headers", "filename");
+    res.setHeader("Content-Type", "audio/mpeg");
+    res.setHeader("filename", fileName);
+    res.setHeader("Content-Disposition", `attachment;\ filename="${fileName}"`);
 
-			readStream.pipe(res);
-		}
-	});
+    readStream.pipe(res);
+  });
 });
 
 export default router;
